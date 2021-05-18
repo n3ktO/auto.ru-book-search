81902c0e1d3881e81dfb271f9c6bfc30b1d1cc17
n3ktO <ruslannekto@gmail.com>
2021-05-18 14:18:22 +0300

    Add loading and selected book reducers.

--- src/state/reducers/index.tsx
+++ src/state/reducers/index.tsx
@@ -2,8 +2,12 @@ import { combineReducers } from 'redux';
 
 import queryReducer from "./queryReducer";
 import booksReducer from "./booksReducer";
+import loadingReducer from "./loadingReducer";
+import selectedBookReducer from "./selectedBookReducer";
 
 export default combineReducers({
   query: queryReducer,
-  books: booksReducer
+  books: booksReducer,
+  loading: loadingReducer,
+  selectedBook: selectedBookReducer
 });
