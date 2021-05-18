469c697a838803128f8054cd61575adad4657aa7
n3ktO <ruslannekto@gmail.com>
2021-05-18 14:12:49 +0300

    Add loading and selected book states.

--- src/state/initialState.tsx
+++ src/state/initialState.tsx
@@ -1,11 +1,15 @@
 type InitialStateType = {
   query: string;
   books: any[];
+  loading: boolean;
+  selectedBook: string;
 };
 
 const initialState: InitialStateType = {
   query: '',
-  books: []
+  books: [],
+  loading: false,
+  selectedBook: null
 };
 
 export default initialState;
