f5d72bc2f39494ec79e153a485c7b61e4877e9c5
n3ktO <ruslannekto@gmail.com>
2021-05-18 21:33:17 +0300

    Finish BookInfoModal component logic.

--- src/components/BookInfoModal.tsx
+++ src/components/BookInfoModal.tsx
@@ -1,10 +1,52 @@
 import React from 'react';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 
-function BookList() {
+import StateType from "../types/StateType";
+
+import { setSelectedBook } from '../state/actions/selectedBookActions';
+
+function getLargeCover(coverId: string) {
+  return `http://covers.openlibrary.org/b/id/${coverId}-L.jpg`;
+}
+
+function BookInfoModal() {
+  const selectedBook: any = useSelector(
+    (state: StateType) => state.selectedBook,
+    shallowEqual
+  );
+
+  const dispatch = useDispatch();
+
   return (
     <div>
+      <div>
+        <div>
+          <img src={getLargeCover(selectedBook['cover_i'])} />
+        </div>
+        <div>{selectedBook['title']}</div>
+        <div>
+          {selectedBook['author_name']
+            .map((author: string) => author)
+            .join(', ')}
+        </div>
+        <div>
+          {selectedBook['first_publish_year'] &&
+            'First publish: ' + selectedBook['first_publish_year']}
+        </div>
+        <div>
+          {selectedBook['publisher'] &&
+            'Publisher: ' + selectedBook['publisher'][0]}
+        </div>
+        <div>
+          {selectedBook['isbn'] &&
+            'ISBN: ' + selectedBook['isbn'][0]}
+        </div>
+      </div>
+      <div>
+        <button onClick={() => dispatch(setSelectedBook(null))}>Close</button>
+      </div>
     </div>
   );
 }
 
-export default BookList;
+export default BookInfoModal;
