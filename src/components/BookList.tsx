050b54eaca4795669ab15415139ea5a97ff9c3e2
n3ktO <ruslannekto@gmail.com>
2021-05-18 21:33:40 +0300

    Finish BookList component logic.

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -3,14 +3,27 @@ import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 
 const Snippet = lazy(() => import('./Snippet'));
 
+import { setSelectedBook } from '../state/actions/selectedBookActions';
+
+import StateType from '../types/StateType';
+
 function BookList() {
-  const books: any[] = useSelector((state: any) => state.books, shallowEqual);
+  const books: any[] = useSelector(
+    (state: StateType) => state.books,
+    shallowEqual
+  );
+
+  const dispatch = useDispatch();
 
   return (
     <div>
       <Suspense fallback={<div>Загрузка...</div>}>
         {books.map((book: any, index: number) => (
-          <Snippet key={index} data={book} />
+          <Snippet
+            key={index}
+            data={book}
+            onClick={() => dispatch(setSelectedBook(book))}
+          />
         ))}
       </Suspense>
     </div>
